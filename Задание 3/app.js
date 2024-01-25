0aff5f433ed2c6d2b81a513ea7d9a47da2dee8b0
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-01-25 10:29:43 +0300

    Выполнили работу с массивами

---
+++ Задание 3/app.js
@@ -0,0 +1,3 @@
+let br = [["Irratech","Misosoft","AWD","Intem","Nvid"],["Textil","BW","New view","Your name","House of mode"],["Jawa","C@","CC+","Snake","Overflow"],["Hi","Bye","Please","help","Привет"]];
+console.log(br);
+console.log(br[1][0]);
