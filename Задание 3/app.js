1ac6146f0589addeb27623f35514583cbd4a7969
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-01-25 13:36:56 +0300

    Выполнили работу с объектами

--- Задание 3/app.js
+++ Задание 3/app.js
@@ -1,3 +1,23 @@
-let br = [["Irratech","Misosoft","AWD","Intem","Nvid"],["Textil","BW","New view","Your name","House of mode"],["Jawa","C@","CC+","Snake","Overflow"],["Hi","Bye","Please","help","Привет"]];
-console.log(br);
-console.log(br[1][0]);
+let colledge = [
+    {
+        sname: "Max",
+        course: 3,
+        ocenki: [4,5,3,4,2],
+        pares:{
+            first: "БЖ",
+            second: "Физра"
+        }
+    },
+    {
+        pname: "Max",
+        age: 33,
+        courses: [2,3],
+        pares: {
+            first: 201,
+            second: 303
+        }
+    }
+]
+console.log(colledge);
+console.log(colledge[0]["sname"],colledge[0]["course"],colledge[0]["pares"]);
+console.log(colledge[1]["pname"],colledge[1]["pares"]);
