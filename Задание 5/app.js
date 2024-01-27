8db4475417e805af51c9cef27b9f88c888fd025f
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-01-27 18:12:21 +0300

    Выполнили функции с for(объекты)

--- Задание 5/app.js
+++ Задание 5/app.js
@@ -1,13 +1,32 @@
-// const people =["Том","Нова","Сэм","Боб","Нат","Иван","Стас"];
-// for(let i=0; i<people.length; i++) {
-//     let result = people[i]
-//     console.log(result);
+// const animals = ["Собака","Кошка","Свинья"]
+// for(let animal of animals){
+//     console.log(animal);
 // }
-const people =[["Tom","Green",33,"New-York"],["Bob","Anderson",25,"Missuri"],["Stanly","Parable",43,"Unknown"],["Kate","Lockwell",24,"Washington"],["Marry","Thomston",23,"Washington"],["Max","Green",35,"New-York"]];
-for(let i=0; i<people.length; i++){
-    for (let j=0; j<people[i].length; j++){
-        let result = people[i][j];
-        console.log(result);
+// let students = {name:"Harry",age:33,course:4}
+// for(let student in students){
+//     console.log(student,students[student]);
+// }
+// const personArray = [
+//     {name: "Tom",age:25},
+//     {name: "Bob",age:23}
+// ]
+// for (let person of personArray) {
+//     for(let key in person){
+//         console.log(key + ": "+person[key]);
+//     }
+// }
+const students =[
+    {name:"Amber",age:16,course:3,love_pares:"Математика",pares_now:"История",ocenki:[4,5,4],date:"12.02.2034"},
+    {name:"Max",age:15,course:2,love_pares:"Английский",pares_now:"Математика",ocenki:[3,3,3],date:"12.02.2034"},
+    {name:"Mike",age:19,course:4,love_pares:"ИЗО",pares_now:"История",ocenki:[3,2,2],date:"12.02.2034"},
+    {name:"Denis",age:18,course:3,love_pares:"Физра",pares_now:"Английский",ocenki:[4,2,4],date:"12.02.2034"},
+    {name:"Mike",age:18,course:4,love_pares:"История",pares_now:"ИЗО",ocenki:[5,2],date:"12.02.2034"},
+    {name:"Max",age:18,course:4,love_pares:"Физра",pares_now:"Физра",ocenki:[4,3,3],date:"12.02.2034"},
+    {name:"Artem",age:15,course:2,love_pares:"Математика",pares_now:"Математика",ocenki:[5,4,5],date:"12.02.2034"}
+]
+for (let student of students) {
+    for(let key in student){
+        console.log(key + ": "+student[key]);
     }
-    console.log("===================================");
+    console.log("=======================================================");
 }
