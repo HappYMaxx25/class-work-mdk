e2a3871701d0c501f296f226b597d0819dc77b09
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-01-27 16:50:30 +0300

    Выполнили функции с for(Двухмерный массив)

--- Задание 5/app.js
+++ Задание 5/app.js
@@ -6,7 +6,7 @@
 const people =[["Tom","Green",33,"New-York"],["Bob","Anderson",25,"Missuri"],["Stanly","Parable",43,"Unknown"],["Kate","Lockwell",24,"Washington"],["Marry","Thomston",23,"Washington"],["Max","Green",35,"New-York"]];
 for(let i=0; i<people.length; i++){
     for (let j=0; j<people[i].length; j++){
-        let result = people[i][j]
+        let result = people[i][j];
         console.log(result);
     }
     console.log("===================================");
