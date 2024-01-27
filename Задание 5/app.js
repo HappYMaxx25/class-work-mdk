32ff0f2418240d34296362b80ac1642d5aa40a4c
Raf228A <rafaelavetisan9@gmail.com>
2024-01-27 16:48:14 +0300

    Выполнили функции с for(Двухмерный массив)

--- Задание 5/app.js
+++ Задание 5/app.js
@@ -1,5 +1,13 @@
-const people =["Том","Нова","Сэм","Боб","Нат","Иван","Стас"];
-for(let i=0; i<people.length; i++) {
-    let result = people[i]
-    console.log(result);
+// const people =["Том","Нова","Сэм","Боб","Нат","Иван","Стас"];
+// for(let i=0; i<people.length; i++) {
+//     let result = people[i]
+//     console.log(result);
+// }
+const people =[["Tom","Green",33,"New-York"],["Bob","Anderson",25,"Missuri"],["Stanly","Parable",43,"Unknown"],["Kate","Lockwell",24,"Washington"],["Marry","Thomston",23,"Washington"],["Max","Green",35,"New-York"]];
+for(let i=0; i<people.length; i++){
+    for (let j=0; j<people[i].length; j++){
+        let result = people[i][j]
+        console.log(result);
+    }
+    console.log("===================================");
 }
