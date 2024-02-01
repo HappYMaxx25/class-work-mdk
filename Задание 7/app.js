a4ce127eb4ff963d6ba19ea91b073e427597c7ff
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-02-01 11:19:39 +0300

    Выполнили Задание 7(Часть 1)

--- Задание 7/app.js
+++ Задание 7/app.js
@@ -1,9 +1,11 @@
-function task3(a,b){
-    if(a>b){
-        console.log(a);
+function task1(a,b,c,d,e,f){
+    mas =[a,b,c,d,e,f]
+    mass =[]
+    for (let i = 0; i < mas.length; i++) {
+        if(mas[i] % 3 === 0 && mas[i] % 5 === 0 && mas[i] % 7 === 0){
+            mass = [mas[i]]
+        }
     }
-    else if(a<b){
-        console.log(b);
-    }
+    console.log(mass);
 }
-task3(8,64)
+task1(105,365,1,65,958,26)
