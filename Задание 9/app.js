95ea7de888410e6ce9e96ffbf715c29100d75929
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-02-03 16:52:12 +0300

    Выполнили Задание 9(Часть 2)

--- Задание 9/app.js
+++ Задание 9/app.js
@@ -1,29 +1,16 @@
-class task1{
-    a
-    b
-    c
-    d
-    e
+class task2{
     i
-    mas
     mass
-    constructor(aa,bb,cc,dd,ee){
-        this.a = aa
-        this.b = bb
-        this.c = cc
-        this.d = dd
-        this.e = ee
-    }
     print(){
-        this.mas=[this.a,this.b,this.c,this.d,this.e]
-        this.mass = []
-        for (this.i = 0; this.i < this.mas.length; this.i++) {
-            if(this.mas[this.i] % 3 === 0 && this.mas[this.i] % 5 === 0 && this.mas[this.i] % 7 === 0 ){
-                this.mass = [this.mas[this.i]];
+        this.mass =[]
+        for (this.i = 0; this.i <= 1000; this.i++) {
+            if(this.i % 3 === 0 && this.i % 5 === 0 && this.i % 7 === 0 ){
+                this.mass = [this.i];
+                console.log(this.mass);
             }
         }
-        console.log(this.mass);
+        
     }
 }
-const result = new task1(105,365,1,65,35)
+const result = new task2()
 result.print()
