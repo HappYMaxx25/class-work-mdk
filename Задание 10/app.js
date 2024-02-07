06672e20236df95a90239bba99dfb59bc994b958
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-02-07 09:29:31 +0300

    Выполнили Задание 10(Часть 3)

--- Задание 10/app.js
+++ Задание 10/app.js
@@ -1,19 +1,35 @@
-class task2{
-    a //начало
-    b //прыжок
-    c //конец
+class task3{
+    a
+    b
+    c
+    d
+    x1
+    x2
     constructor(aa,bb,cc){
         this.a = aa
         this.b = bb
         this.c = cc
     }
+    Dis(){
+        this.d = Math.pow(this.b,2)-(4*this.a*this.c)
+        return this.d
+    }
     print(){
-        let res=0;
-        for(let i = this.a; i <= this.c; i=i+this.b){
-            res = res + i
+        if(this.Dis()>0){
+            this.x1= (-this.b - Math.sqrt(this.Dis())) / (2*this.a)
+            this.x2= (-this.b + Math.sqrt(this.Dis())) / (2*this.a)
+            console.log(`Решения квадратного уравнения ${this.a}x^2 + ${this.b}x + ${this.c}= 0 --> x1=${this.x1}; x2=${this.x2}`);
         }
-        console.log(`Арифметическая прогрессия = ${res}`);
+        else if(this.Dis()===0){
+            this.x1=-(this.b/(2*this.a))
+            this.x2=0
+            console.log(`Решения квадратного уравнения ${this.a}x^2 + ${this.b}x + ${this.c}= 0 --> x1=${this.x1}`);
+        }
+        else if(this.Dis()<0){
+            console.log(`Решения не найдено`);
+        }
+        
     }
 }
-const result = new task2(1,5,10)
+const result = new task3(1,2,3)
 result.print()
