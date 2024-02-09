3c637f2a14efa9b086f33b1d60d7c5241a43af42
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-02-09 13:35:05 +0300

    Выполнили Задание 12(DOM-элементы)

--- Задание 12/app.js
+++ Задание 12/app.js
@@ -1,3 +1,22 @@
 //DOM
-const text = document.querySelector(".block .text")
-console.log(text.innerText);
+const text = document.querySelector(".text")
+const net = document.querySelector(".bred")
+let i = text.innerText
+let u = net.innerText
+let p = i/10*100
+console.log(`Вы взяли кредит на ${i} на ${u} года(лет). Вы должны вернуть ${p}`);
+// for (i of text) {
+//     if(i.innerText === "Winter"){
+//         console.log("Надевай шапку");
+//     }else if(i.innerText === "Sping"){
+//         console.log("Надевай пальто");
+//     }
+//     else if(i.innerText === "Summer"){
+//         console.log("Надевай кепку");
+//     }
+//     else if(i.innerText === "Awtumn"){
+//         console.log("Надевай носки");
+//     }
+    
+// }
+//2 эемента размер кредита  кол-во лет(1-5) форма кредита вывести на сколько лет + переплата
