db2074a9c34c616f5e4465855d0612231c7a92cc
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-02-13 10:34:10 +0300

    Выполнили Задание 13 (Расчёт зарплаты)

--- Задание 13/app.js
+++ Задание 13/app.js
@@ -1,11 +1,16 @@
-const text = document.querySelector(".text")
-const net = document.querySelector(".bred")
-let i = text.innerText
-let u = net.innerText
-let p = i/10*100
-if(i>100000){
-    console.log(`Вы взяли кредит на ${i} на ${u} года(лет). Вы должны вернуть ${p}`);
-}
-else{
-    console.log(`Нужно больше 100000`);
-}
+let inputOne=document.querySelector('#input-1')
+let inputTwo=document.querySelector('#input-2')
+let btn=document.querySelector('#btn')
+let textResult=document.querySelector('#result-text')
+btn.addEventListener('click',()=>{
+    if(inputOne.value.length === 0 || inputTwo.value.length === 0){
+        alert(`Заполнните все  поля!`)
+    }else{
+        if(inputTwo.value > 31){
+            alert(`Укажите конкретное кол-во дней`)
+        }else{
+            let result = inputOne.value * inputTwo.value
+            textResult.innerText = `За ${inputTwo.value} дня(дней), Вы заработайте ${result} рублей`
+        }
+    }
+})
