688d58a87d40d0c135f8963b08ea15d882ed6056
HappYMaxx25 <maxx.rock0678@mail.ru>
2024-02-21 09:09:08 +0300

    Задание 16 (Дистанционка)

---
+++ Задание 16/app.js
@@ -0,0 +1,19 @@
+let summ
+let otpName
+let polName
+let nextBtn
+let reqInput = () => {
+    summ = document.querySelector("#input-summ").value
+    otpName=document.querySelector("#input-otp_name").value
+    polName=document.querySelector("#input-pol_name").value
+    nextBtn = document.querySelector("#next-btn")
+    if ( summ.length !=0 && otpName.length !=0 && polName.length !=0) {
+        nextBtn.disabled = false
+    }
+    return [summ]
+}
+let saveForm=() =>{
+    let operation = reqInput()
+    alert(`Операция на ${summ} успешно проведена`)
+
+}
